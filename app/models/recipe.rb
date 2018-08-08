917df4772f02121302b2bfbd2d3592401b42ca89
Flatiron School <flatiron@Flatirons-MacBook-Air.local>
2018-08-08 09:05:25 +0100

    Jo

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,9 @@
-#Placeholder for a model
+class Recipe<ActiveRecord::Base
+
+  # attr_accessor :name, :ingredients, :cook_time
+  #
+  # def initialize(name: name, ingredients: ingredients, cook_time: cook_time)
+  #
+  # end
+
+end
